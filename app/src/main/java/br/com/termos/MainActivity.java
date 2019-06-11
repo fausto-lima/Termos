ef9a547f0bb3eb9852ae241d68229dd22c9aae9f
fausto.sys@gmail.com <fausto.sys@gmail.com>
2019-06-11 15:10:08 -0300

    Rede 11/06

--- app/src/main/java/br/com/termos/MainActivity.java
+++ app/src/main/java/br/com/termos/MainActivity.java
@@ -1,20 +1,39 @@
 package br.com.termos;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v4.widget.CircularProgressDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressDialog pd;
+
     private TextView tvTermos ;
     private TextView tvContrato ;
     private TextView tvTermosAtual ;
     private TextView tvContratoAtual ;
 
+    private Context context;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy =new
+        StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        context = this;
+        pd = new ProgressDialog(context);
+        if (pd.isShowing()){
+            pd.dismiss();
+        }
+
         setTitle(R.string.app_name);
         setContentView(R.layout.activity_main);
         tvTermos = (TextView) findViewById(R.id.tvTermos);
@@ -24,48 +43,50 @@ public class MainActivity extends AppCompatActivity {
         tvTermos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                pd.show();
                 Intent intent = new Intent(MainActivity.this, webview.class);
                 Bundle b = new Bundle();
                 b.putString("url", "http://adroot1.ddns.net:3355/Privacidade.htm"); //Your id
                 intent.putExtras(b); //Put your id to your next Intent
                 startActivity(intent);
-                //finish();
 
             }
         });
         tvContrato.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                pd.show();
                 Intent intent = new Intent(MainActivity.this, webview.class);
                 Bundle b = new Bundle();
                 b.putString("url", "http://adroot1.ddns.net:3355/Termos.htm"); //Your id
                 intent.putExtras(b); //Put your id to your next Intent
                 startActivity(intent);
-
             }
         });
         tvTermosAtual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                pd.show();
                 Intent intent = new Intent(MainActivity.this, webview.class);
                 Bundle b = new Bundle();
                 b.putString("url", "https://www.userede.com.br/pt-BR/Documents/MobileRede/PoliticaPrivacidade.htm"); //Your id
                 //b.putString("url", "http://adroot1.ddns.net:3355/Privacidade.htm"); //Your id
                 intent.putExtras(b); //Put your id to your next Intent
                 startActivity(intent);
-                //finish();
 
             }
         });
         tvContratoAtual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                pd.show();
                 Intent intent = new Intent(MainActivity.this, webview.class);
                 Bundle b = new Bundle();
                 b.putString("url", "https://www.userede.com.br/TermosMobileRede/Paginas/Contrato_Credenciamento_Adesao.htm"); //Your id
                 intent.putExtras(b); //Put your id to your next Intent
                 startActivity(intent);
-
             }
         });
     }
--- app/src/main/java/br/com/termos/webview.java
+++ app/src/main/java/br/com/termos/webview.java
@@ -1,23 +1,29 @@
 package br.com.termos;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
-
+import android.widget.ProgressBar;
 
 public class webview extends AppCompatActivity {
+    ProgressDialog pd;
+
     private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        pd = new ProgressDialog(this);
         setContentView(R.layout.activity_webview);
         Bundle b = getIntent().getExtras();
         String url = "http://www.google.com"; // or other values
-        if(b != null)
+        if(b != null) {
             url = b.getString("url");
+            pd.dismiss();
+        }
         webView = findViewById(R.id.webview);
         webView.loadUrl(url);
-
-
+        pd.dismiss();
     }
 }
